0832040ac07a1caaafc1b479a6cf4480902eb497
AndresMarolt <andresmmarolt@gmail.com>
2023-03-21 19:46:32 -0300

    Final changes

--- src/app/services/web-request.service.ts
+++ src/app/services/web-request.service.ts
@@ -9,7 +9,7 @@ export class WebRequestService {
   readonly ROOT_URL;
 
   constructor(private httpClient: HttpClient) {
-    this.ROOT_URL = 'http://192.168.0.8:8000'
+    this.ROOT_URL = 'https://task-manager-backend-azure.vercel.app/'
   }
 
   get(uri: string) {
